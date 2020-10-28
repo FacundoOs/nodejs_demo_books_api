319c889034c1a1aece2351805ac61ccade1995c0
FacundoOs <osores.facundo@gmail.com>
2020-10-28 12:49:53 +0100

    Add get(index) and post(create) for do the requests. Create the post request and retrieve the query string with request.query to return a JS object after the query string is parsed. Add nodemon for hot-reload and a script to open it

--- app.js
+++ app.js
@@ -7,11 +7,14 @@ const { response } = require("express");
 const app = express();
 
 app
+  // middleware for parsing json objects. Looks at requests where the Content-Type: application/json header is present and transforms the text-based JSON input into JS-accessible variables under req.body.
   .use(bodyParser.json())
+  // middleware for parsing bodies from URL. Does the same for URL-encoded requests. the extended: true precises that the req.body object will contain values of any type instead of just strings.
   .use(bodyParser.urlencoded({ extended: true }))
+  //CORS is a node.js package for providing a Connect/Express middleware that can be used to enable CORS with various options.
   .use(cors());
 
-const getBooks = (request, response) => {
+const index = (request, response) => {
   pool.query("SELECT * FROM books", (error, results) => {
     if (error) {
       throw error;
@@ -20,7 +23,21 @@ const getBooks = (request, response) => {
   });
 };
 
-app.route("/books").get(getBooks);
+const create = (request, response) => {
+  const { author, title } = request.query;
+  pool.query(
+    "INSERT INTO books (author, title) VALUES ($1, $2)",
+    [author, title],
+    (error) => {
+      if (error) {
+        throw error;
+      }
+      response.status(201).json({ message: "Book was added to the database!" });
+    }
+  );
+};
+
+app.route("/books").get(index).post(create);
 
 app.listen(process.env.PORT || 3002, () => {
   console.log("The server is listening...");
